d60d0e85c5b8b399544459c5821bb1ad981e4553
Akshay Shah <meetakshay99@gmail.com>
2025-08-26 12:56:06 +0530

    Fix for https://github.com/trulience/client/issues/558

--- livekit-agents/livekit/agents/tokenize/_basic_sent.py
+++ livekit-agents/livekit/agents/tokenize/_basic_sent.py
@@ -8,6 +8,25 @@ def split_sentences(
     """
     the text may not contain substrings "<prd>" or "<stop>"
     """
+    
+    # Step 1: Protect XML tags and their attributes
+    xml_placeholders = {}
+    xml_counter = 0
+    
+    # Find all XML-like tags (including self-closing ones)
+    xml_pattern = r'<[a-zA-Z][^>]*/?>'
+    
+    def replace_xml(match):
+        nonlocal xml_counter
+        placeholder = f"__XML_PLACEHOLDER_{xml_counter}__"
+        xml_placeholders[placeholder] = match.group(0)
+        xml_counter += 1
+        return placeholder
+    
+    # Replace XML tags with placeholders
+    protected_text = re.sub(xml_pattern, replace_xml, text)
+    
+    # Step 2: Apply existing sentence splitting logic to protected text
     alphabets = r"([A-Za-z])"
     prefixes = r"(Mr|St|Mrs|Ms|Dr)[.]"
     suffixes = r"(Inc|Ltd|Jr|Sr|Co)"
@@ -19,38 +38,48 @@ def split_sentences(
 
     # fmt: off
     if retain_format:
-        text = text.replace("\n","<nel><stop>")
+        protected_text = protected_text.replace("\n","<nel><stop>")
     else:
-        text = text.replace("\n"," ")
+        protected_text = protected_text.replace("\n"," ")
 
-    text = re.sub(prefixes,"\\1<prd>", text)
-    text = re.sub(websites,"<prd>\\1", text)
-    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
-    # text = re.sub(multiple_dots, lambda match: "<prd>" * len(match.group(0)) + "<stop>", text)
+    protected_text = re.sub(prefixes,"\\1<prd>", protected_text)
+    protected_text = re.sub(websites,"<prd>\\1", protected_text)
+    protected_text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2", protected_text)
+    # protected_text = re.sub(multiple_dots, lambda match: "<prd>" * len(match.group(0)) + "<stop>", protected_text)
     # TODO(theomonnom): need improvement for ""..." dots", check capital + next sentence should not be  # noqa: E501
     # small
-    text = re.sub(multiple_dots, lambda match: "<prd>" * len(match.group(0)), text)
-    if "Ph.D" in text:
-        text = text.replace("Ph.D.","Ph<prd>D<prd>")
-    text = re.sub(r"\s" + alphabets + "[.] "," \\1<prd> ",text)
-    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)  # noqa: E501
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
-    text = re.sub(r" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-    text = re.sub(r" "+suffixes+"[.]"," \\1<prd>",text)
-    text = re.sub(r" " + alphabets + "[.]"," \\1<prd>",text)
+    protected_text = re.sub(multiple_dots, lambda match: "<prd>" * len(match.group(0)), protected_text)
+    if "Ph.D" in protected_text:
+        protected_text = protected_text.replace("Ph.D.","Ph<prd>D<prd>")
+    protected_text = re.sub(r"\s" + alphabets + "[.] "," \\1<prd> ", protected_text)
+    protected_text = re.sub(acronyms+" "+starters,"\\1<stop> \\2", protected_text)
+    protected_text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>", protected_text)  # noqa: E501
+    protected_text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>", protected_text)
+    protected_text = re.sub(r" "+suffixes+"[.] "+starters," \\1<stop> \\2", protected_text)
+    protected_text = re.sub(r" "+suffixes+"[.]"," \\1<prd>", protected_text)
+    protected_text = re.sub(r" " + alphabets + "[.]"," \\1<prd>", protected_text)
 
     # mark end of sentence punctuations with <stop>
-    text = re.sub(r"([.!?。！？])([\"”])", "\\1\\2<stop>", text)
-    text = re.sub(r"([.!?。！？])(?![\"”])", "\\1<stop>", text)
+    protected_text = re.sub(r"([.!?。！？])([\"\"''])", "\\1\\2<stop>", protected_text)
+    protected_text = re.sub(r"([.!?。！？])(?![\"\"''])", "\\1<stop>", protected_text)
 
-    text = text.replace("<prd>",".")
+    protected_text = protected_text.replace("<prd>",".")
     # fmt: on
 
     if retain_format:
-        text = text.replace("<nel>", "\n")
-    splitted_sentences = text.split("<stop>")
-    text = text.replace("<stop>", "")
+        protected_text = protected_text.replace("<nel>", "\n")
+    
+    # Step 3: Restore XML placeholders before splitting
+    def restore_xml(match):
+        placeholder = match.group(0)
+        return xml_placeholders.get(placeholder, placeholder)
+    
+    # Restore XML tags
+    restored_text = re.sub(r'__XML_PLACEHOLDER_\d+__', restore_xml, protected_text)
+    
+    # Split sentences
+    splitted_sentences = restored_text.split("<stop>")
+    final_text = restored_text.replace("<stop>", "")
 
     sentences: list[tuple[str, int, int]] = []
 
@@ -74,6 +103,6 @@ def split_sentences(
             buff = ""
 
     if buff:
-        sentences.append((buff[len(pre_pad) :], start_pos, len(text) - 1))
+        sentences.append((buff[len(pre_pad) :], start_pos, len(final_text) - 1))
 
     return sentences
